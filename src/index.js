439c9a14892bd54e75a6161fa670409d88253118
SarahMumbua <mumbuasarah2030@gmail.com>
2023-04-04 00:09:31 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+const taskForm = document.querySelector('#create-task-form');
+const taskInput = document.querySelector('#new-task-description');
+const taskList = document.querySelector('#tasks');
+function handleTaskFormSubmit(event) {
+  event.preventDefault();
+  const taskText = taskInput.value;
+  const listItem = document.createElement('li');
+  listItem.innerText = taskText;
+  taskList.appendChild(listItem);
+  taskInput.value = '';
+}
+taskForm.addEventListener('submit', handleTaskFormSubmit);
+
 });
